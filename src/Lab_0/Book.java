57d9625cf40e489334c2916de45584622fee17b1
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-12 20:06:24 +0300

    New class for books

---
+++ src/Lab_0/Book.java
@@ -0,0 +1,19 @@
+package Lab_0;
+
+public class Book {
+    private String title;
+    private double price;
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
