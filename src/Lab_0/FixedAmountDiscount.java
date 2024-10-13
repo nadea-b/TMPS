a03fc8b9baf074144a063acd9b93d50c8c11b4b2
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-13 16:09:58 +0300

    Implementations of the interface

--- src/Lab_0/FixedAmountDiscount.java
+++ src/Lab_0/FixedAmountDiscount.java
@@ -1,2 +1,14 @@
-package Lab_0;public class FixedAmountDiscount {
+package Lab_0;
+
+public class FixedAmountDiscount implements Discount {
+    private double amount;
+
+    public FixedAmountDiscount(double amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        return total - amount;  // e.g., subtract $5
+    }
 }
--- src/Lab_0/PercentageDiscount.java
+++ src/Lab_0/PercentageDiscount.java
@@ -1,2 +1,14 @@
-package Lab_0;public class PercentageDiscount {
+package Lab_0;
+
+public class PercentageDiscount implements Discount {
+    private final double percentage;
+
+    // Constructor to initialize the discount percentage
+    public PercentageDiscount(double percentage) {
+        this.percentage = percentage;
+    }
+
+    @Override
+    public double applyDiscount(double total) {
+        return total - (total * percentage / 100);}
 }
