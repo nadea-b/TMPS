cc563ebdf6ec5c72bb600e04e8a4b20390a99a82
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-13 15:17:10 +0300

    Main function added

--- src/Lab_0/Main.java
+++ src/Lab_0/Main.java
@@ -1,4 +1,19 @@
 package Lab_0;
 
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("Simple Book", 10.00);
+        Book book2 = new Book("Another Book", 20.00);
+
+        Cart cart = new Cart();
+        cart.addBook(book1);
+        cart.addBook(book2);
+
+        System.out.println("Total before discount: $" + cart.getTotal());
+
+        Discount discount = new Discount();
+        double discountedTotal = discount.applyDiscount(cart.getTotal());
+
+        System.out.println("Total after discount: $" + discountedTotal);
+    }
 }
