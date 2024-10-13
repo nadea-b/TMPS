180d70d23f21bdb5ad60eb2daf5a186f8fb2ea24
Nadejda Barbarov <nadejda.barbarov@isa.utm.com>
2024-10-13 15:16:20 +0300

    Calculates discounts and other types can be added

---
+++ src/Lab_0/Discount.java
@@ -0,0 +1,9 @@
+package Lab_0;
+
+public class Discount {
+    public double applyDiscount(double total) {
+        // Open for different discount types (percentage, fixed, etc.)
+        return total * 0.9; // 10% discount
+    }
+}
+
